0d484f2e5215fc09642a78205915586b4a2eadfd
L9029 <angeloca03@gmail.com>
2025-08-13 16:04:46 -0400

    Se procedio a crear el metodo login en el controlador LoginController que se encarga de validar los datos del usuario para el uso de la API y la respectiva generación de su token unico.

--- app/Http/Controllers/Api/LoginController.php
+++ app/Http/Controllers/Api/LoginController.php
@@ -9,5 +9,44 @@ use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
-    //
+    /**
+     * Maneja el inicio de sesión de un usuario y genera un token de acceso.
+     *
+     * Este método valida las credenciales del usuario (email y contraseña) y, si son correctas,
+     * genera un token de acceso utilizando Laravel Sanctum. El token se asocia al dispositivo
+     * especificado en la solicitud.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function login(Request $request) 
+    {
+        $this->validateLogin($request);
+
+        if (Auth::attempt($request->only(['email', 'password']))) {
+            return response()->json([
+                'token' => $request->user()->createToken($request->device)->plainTextToken,
+                'message' => 'Success',
+            ]);
+        }
+
+        return response()->json([
+            'message' => 'Unauthorized'
+        ], 401);
+    }
+
+    /**
+     * Valida los datos de la solicitud de inicio de sesión.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function validateLogin(Request $request) 
+    {
+        return $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+            'device' => 'required',
+        ]);
+    }
 }
